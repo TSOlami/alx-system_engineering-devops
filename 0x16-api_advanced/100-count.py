d6081f67a599bd4c796cd2d03684ea3d74b50ff9
TSOlami <tijani.saheed@yahoo.com>
2023-06-08 23:11:51 +0100

    Fixed 100-count.py to print the correct output

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -3,63 +3,63 @@
 This script queries the Reddit API
 and returns the number of subscribers
 for a given subreddit.
-If an invalid subreddit is given,
-the function will return 0.
 """
+from requests import get
 
-import json
-import requests
+REDDIT = "https://www.reddit.com/"
+HEADERS = {'user-agent': 'my-app/0.0.1'}
 
 
-def count_words(subreddit, word_list, after=None, params=None, count=[]):
+def count_words(subreddit, word_list, after="", word_dic={}):
     """
+    Returns a list containing the titles of all hot articles for a
+    given subreddit. If no results are found for the given subreddit,
+    the function should return None.
     """
-    if after == "":
-        count = [0] * len(word_list)
+    if not word_dic:
+        for word in word_list:
+            word_dic[word] = 0
 
-    # Set user agent and url
-    user_agent = {'User-agent': 'tsolami'}
-    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    if after:
-        params = {'after': after}
-    response = requests.get(
-        url,
-        headers=user_agent,
-        params=params,
-        allow_redirects=False)
+    if after is None:
+        word_list = [[key, value] for key, value in word_dic.items()]
+        word_list = sorted(word_list, key=lambda x: (-x[1], x[0]))
+        for w in word_list:
+            if w[1]:
+                print("{}: {}".format(w[0].lower(), w[1]))
+        return None
 
-    if response.status_code == 200:
-        data = response.json()
-        for topic in (data['data']['children']):
-            for word in topic['data']['title'].split():
-                for i in range(len(word_list)):
-                    if word_list[i].lower() == word.lower():
-                        count[i] += 1
+    url = REDDIT + "r/{}/hot/.json".format(subreddit)
 
-        after = data['data']['after']
-        if after is None:
-            save = []
-            for i in range(len(word_list)):
-                for j in range(i + 1, len(word_list)):
-                    if word_list[i].lower() == word_list[j].lower():
-                        save.append(j)
-                        count[i] += count[j]
+    params = {
+        'limit': 100,
+        'after': after
+    }
 
-            for i in range(len(word_list)):
-                for j in range(i, len(word_list)):
-                    if (count[j] > count[i] or
-                            (word_list[i] > word_list[j] and
-                             count[j] == count[i])):
-                        aux = count[i]
-                        count[i] = count[j]
-                        count[j] = aux
-                        aux = word_list[i]
-                        word_list[i] = word_list[j]
-                        word_list[j] = aux
+    r = get(url, headers=HEADERS, params=params, allow_redirects=False)
 
-            for i in range(len(word_list)):
-                if (count[i] > 0) and i not in save:
-                    print("{}: {}".format(word_list[i].lower(), count[i]))
-        else:
-            count_words(subreddit, word_list, after, count)
+    if r.status_code != 200:
+        return None
 
+    try:
+        js = r.json()
+
+    except ValueError:
+        return None
+
+    try:
+
+        data = js.get("data")
+        after = data.get("after")
+        children = data.get("children")
+        for child in children:
+            post = child.get("data")
+            title = post.get("title")
+            lower = [s.lower() for s in title.split(' ')]
+
+            for w in word_list:
+                word_dic[w] += lower.count(w.lower())
+
+    except:
+        return None
+
+    count_words(subreddit, word_list, after, word_dic)
